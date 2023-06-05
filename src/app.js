4bfcf320a4d9345b79b4add481a2f48ecb6a1395
Xinawn <egor.berezovskiy.2017@mail.ru>
2023-06-05 17:28:00 +0300

    d
    de

--- src/app.js
+++ src/app.js
@@ -1,17 +1,11 @@
 const express = require('express');
-// const swaggerUI = require('swagger-ui-express');
-// const path = require('path');
-// const YAML = require('yamljs');
 const abiturientsRouter = require('./resources/abiturients/abiturients.router');
 const teacherRouter = require('./resources/teachers/teachers.router');
 const examsRouter = require('./resources/exams/exams.router');
 
 const app = express();
-// const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
 app.use(express.json());
-
-// app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use('/', (req, res, next) => {
   if (req.originalUrl === '/') {
